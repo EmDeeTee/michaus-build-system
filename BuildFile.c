9255fe968ec41c0b43fff2563568fc32b21672ed
EmDeeTee <m12@onet.com.pl>
2023-01-02 23:08:33 +0000

    Added compiler choice and flags

--- BuildFile.c
+++ BuildFile.c
@@ -2,27 +2,46 @@
 #include "Parser.h"
 
 extern char FilesToBuild[1024];
+extern char CompilerFlags[1024];
+extern char CompilerToUse[128];
 
 void ReadBuildFile() {
     config_option_t co;
-        if ((co = read_config_file("mbs")) == NULL) {
-            perror("Can't find the 'mbs' file! I'm giving up :(");
-            exit(-1);
-        }
-        while(1) {
-            //printf("Key: %s\nValue: %s\n", co->key, co->value);
+    if ((co = read_config_file("mbs")) == NULL) {
+        perror("Can't find the 'mbs' file! I'm giving up :(");
+        exit(-1);
+    }
+    while(1) {
+        //printf("Key: %s\nValue: %s\n", co->key, co->value);
+        if (strcmp(co->key, "FILES") == 0)
             strcpy(FilesToBuild, co->value);
-            for(int i=0;FilesToBuild[i]!='\0';i++) {
-                if(FilesToBuild[i]==',') {
-                        FilesToBuild[i] = ' ';
-                }
-                else
-                        continue;
-            }
-            if (co->prev != NULL) {
-                co = co->prev;
-            } else {
-                break;
-            }
+        if (strcmp(co->key, "COMPILER") == 0) {
+            strcpy(CompilerToUse, co->value);
+            strcat(CompilerToUse, " ");
         }
+        if (strcmp(co->key, "FLAGS") == 0) {
+            strcpy(CompilerFlags, co->value);
+            strcat(CompilerFlags, " ");
+        }
+
+        // Replace comas for dots in file string and flags string
+        for(int i=0;FilesToBuild[i]!='\0';i++) {
+            if(FilesToBuild[i]==',') {
+                    FilesToBuild[i] = ' ';
+            }
+            else
+                continue;
+        }
+        for(int i=0;CompilerFlags[i]!='\0';i++) {
+            if(CompilerFlags[i]==',') {
+                    CompilerFlags[i] = ' ';
+            }
+            else
+                continue;
+        }
+
+        if (co->prev != NULL) {
+            co = co->prev;
+        } else {break;}
+    }
 }
--- main.c
+++ main.c
@@ -7,24 +7,28 @@
 #include "BuildFile.h"
 
 char FilesToBuild[1024];
+char CompilerFlags[1024];
+char CompilerToUse[128];
 
 int main() {
     printf("Welcome to Michau's Build System - MBS\n");
     ReadBuildFile();
     printf("Files that will be included: %s\n", FilesToBuild);
-    char cmd[255] = "gcc ";
+    printf("Project will be compiled using: %s\n", CompilerToUse);
+    printf("Compiler will get the following flags: %s\n", CompilerFlags);
     char *new_str ;
-    if((new_str = malloc(strlen(cmd)+strlen(FilesToBuild)+1)) != NULL){
+    if((new_str = malloc(strlen(CompilerToUse)+strlen(FilesToBuild)+1)) != NULL){
         new_str[0] = '\0';   // ensures the memory is an empty string
-        strcat(new_str,cmd);
+        strcat(new_str,CompilerToUse);
+        strcat(new_str,CompilerFlags);
         strcat(new_str,FilesToBuild);
-
     } else {
         perror("malloc failed! AAAAAAAAAAAAAAAAAAAAAAA\n");
         exit(-1);
     }
     printf("Running the compiler...\n");
     system(new_str);
+    free(new_str);
     printf("DONE!\n");
     return 0;
 }
